0d37db1211bde8794712eb20109d950275ef717d
yopres raprap <48550256+yopres94@users.noreply.github.com>
2019-07-08 18:53:07 +0700

    Add files via upload

---
+++ workbox-config.js
@@ -0,0 +1,52 @@
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
+//
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*",
+    "offline.html"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*",
+    "offline.html",
+    "icons/**.*"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
+module.exports = {
+  "globDirectory": "./",
+  "globPatterns": [
+    "img/**.*",
+    "offline.html",
+    "icons/**.*",
+    "shell.html",
+    "js/app.js"
+  ],
+  "swSrc": "src/sw.js",
+  "swDest": "sw.js",
+  "globIgnores": [
+    "./workbox-config.js"
+  ]
+};
